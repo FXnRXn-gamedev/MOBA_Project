6bcd80db360a698702f3420d6faca833ae7aacf7
FXnRXn <frabbi032@gmail.com>
2025-10-28 12:52:09 +0600

    init

---
+++ Source/MOBA_Project/MOBA_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MOBA_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MOBA_Project, "MOBA_Project" );
