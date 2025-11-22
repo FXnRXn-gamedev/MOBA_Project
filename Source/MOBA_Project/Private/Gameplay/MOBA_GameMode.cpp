3dc96b80acb63eb0189c285219bf20167909b4be
FXnRXn <frabbi032@gmail.com>
2025-11-22 10:09:57 +0600

    Init Team ID

--- Source/MOBA_Project/Private/Gameplay/MOBA_GameMode.cpp
+++ Source/MOBA_Project/Private/Gameplay/MOBA_GameMode.cpp
@@ -2,3 +2,47 @@
 
 
 #include "MOBA_Project/Public/Gameplay/MOBA_GameMode.h"
+#include "GameFramework/PlayerStart.h"
+#include "EngineUtils.h"
+
+APlayerController* AMOBA_GameMode::SpawnPlayerController(ENetRole InRemoteRole, const FString& Options)
+{
+	// Spawn Player Controller
+	APlayerController* NewPlayerController = Super::SpawnPlayerController(InRemoteRole, Options);
+	IGenericTeamAgentInterface* NewPlayerTeamInterface = Cast<IGenericTeamAgentInterface>(NewPlayerController);
+	FGenericTeamId TeamId = GetTeamIDForPlayer(NewPlayerController);
+	if (NewPlayerTeamInterface)
+	{
+		NewPlayerTeamInterface->SetGenericTeamId(TeamId);
+	}
+	
+	NewPlayerController->StartSpot = FindNextStartSpotForTeam(TeamId);
+	return NewPlayerController;
+}
+
+FGenericTeamId AMOBA_GameMode::GetTeamIDForPlayer(const APlayerController* PlayerController) const
+{
+	static int PlayerCount = 0;
+	++PlayerCount;
+	return FGenericTeamId(PlayerCount % 2);
+}
+
+AActor* AMOBA_GameMode::FindNextStartSpotForTeam(const FGenericTeamId& TeamID) const
+{
+	const FName* StartSpotTag = TeamStartSpotTagMap.Find(TeamID); 
+	if (!StartSpotTag) return nullptr;
+	
+	UWorld* World = GetWorld();
+	if (!World) return nullptr;
+
+	for (TActorIterator<APlayerStart> It(World); It; ++It)
+	{
+		if (It->PlayerStartTag == *StartSpotTag)
+		{
+			It->PlayerStartTag = FName("Taken");
+			return *It;
+		}
+	}
+	
+	return nullptr;
+}
--- Source/MOBA_Project/Public/Gameplay/MOBA_GameMode.h
+++ Source/MOBA_Project/Public/Gameplay/MOBA_GameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "GenericTeamAgentInterface.h"
 #include "MOBA_GameMode.generated.h"
 
 
@@ -11,4 +12,18 @@ UCLASS()
 class MOBA_PROJECT_API AMOBA_GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+	
+	//------------------------------------------------------------------------------------------------------------------
+public:
+	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, const FString& Options) override;
+	
+	//------------------------------------------------------------------------------------------------------------------
+private:
+	FGenericTeamId GetTeamIDForPlayer(const APlayerController* PlayerController) const;
+	AActor* FindNextStartSpotForTeam(const FGenericTeamId& TeamID) const;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Moba|Team")
+	TMap<FGenericTeamId , FName> TeamStartSpotTagMap;
+	
+	
 };
