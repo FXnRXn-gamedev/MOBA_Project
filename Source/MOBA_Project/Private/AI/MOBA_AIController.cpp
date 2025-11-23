8d8571c67efbe3affbbd181af030e33f8a11c087
FXnRXn <frabbi032@gmail.com>
2025-11-23 08:59:39 +0600

    AI Perception Setup

---
+++ Source/MOBA_Project/Private/AI/MOBA_AIController.cpp
@@ -0,0 +1,54 @@
+﻿// FXnRXn copyright
+
+
+#include "AI/MOBA_AIController.h"
+
+#include "Character/MOBA_CharacterBase.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+//---> CONSTRUCTOR <---
+//-----------------------------------------
+
+
+AMOBA_AIController::AMOBA_AIController()
+{
+	// Create AIPerceptionComponent
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
+	SetPerceptionComponent(*AIPerceptionComponent);
+	
+	// Create SightConfig
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	
+	// Configure SightConfig parameters
+	SightConfig->SightRadius = 1000.0f;
+	SightConfig->LoseSightRadius = 1200.0f;
+	SightConfig->PeripheralVisionAngleDegrees = 180.0f;
+	SightConfig->SetMaxAge(5.0f);
+	
+	// Configure Detection by Affiliation
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	
+	// Add SightConfig to AIPerceptionComponent
+	AIPerceptionComponent->ConfigureSense(*SightConfig);
+}
+
+
+//---> UNREAL CALLBACK <---
+//-----------------------------------------
+
+void AMOBA_AIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	SetGenericTeamId(FGenericTeamId(0));
+	
+	IGenericTeamAgentInterface* PawnAsTeamAgentInterface = Cast<IGenericTeamAgentInterface>(InPawn);
+	if (PawnAsTeamAgentInterface)
+	{
+		PawnAsTeamAgentInterface->SetGenericTeamId(GetGenericTeamId());
+	}
+}
+
---
+++ Source/MOBA_Project/Public/AI/MOBA_AIController.h
@@ -0,0 +1,34 @@
+﻿// FXnRXn copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MOBA_AIController.generated.h"
+
+
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+
+
+
+
+UCLASS()
+class MOBA_PROJECT_API AMOBA_AIController : public AAIController
+{
+	GENERATED_BODY()
+
+	//------------------------------------------------------------------------------------------------------------------
+public:
+	AMOBA_AIController();
+	virtual void OnPossess(APawn* InPawn) override;
+
+	//------------------------------------------------------------------------------------------------------------------
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category="Moba|AIPerception")
+	UAIPerceptionComponent* AIPerceptionComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category="Moba|AIPerception")
+	UAISenseConfig_Sight* SightConfig;
+	
+};
