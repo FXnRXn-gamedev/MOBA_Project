5224d9dbd5ad9b1c992964d86531484eb496a6d0
FXnRXn <frabbi032@gmail.com>
2025-11-25 08:01:22 +0600

    AI Chasing

---
+++ Source/MOBA_Project/Public/AI/BTTask_SendInputToAbilitySystem.h
@@ -0,0 +1,26 @@
+﻿// FXnRXn copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "AbilitySystem/MOBA_GameplayAbilityTypes.h"
+#include "BTTask_SendInputToAbilitySystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBA_PROJECT_API UBTTask_SendInputToAbilitySystem : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	//------------------------------------------------------------------------------------------------------------------
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	//------------------------------------------------------------------------------------------------------------------
+private:
+	UPROPERTY(EditAnywhere, Category = "Moba|Task")
+	EMOBA_AbilityInputID AbilityInputID;
+};
